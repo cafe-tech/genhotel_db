c894ed8eac76caf1f980d7ab07137c57b3a4085b
cafe-tech <kiko.peace.mk@gmail.com>
2017-01-30 01:22:49 +0800

    Add files via upload
    Initial commit

---
+++ 01_Database.sql
@@ -0,0 +1,23 @@
+-- Copyright © 2014-2017, Michael Francisco / FCS. All rights reserved.
+/*
+ GenHotel database scripts for MySQL is licensed under the terms of the GPLv2
+ <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
+
+ This program is free software; you can redistribute it and/or modify 
+ it under the terms of the GNU General Public License as published 
+ by the Free Software Foundation; version 2 of the License.
+
+ This program is distributed in the hope that it will be useful, but 
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ or FITNESS FOR A PARTICULAR BUSINESS MODEL. See the GNU General Public License 
+ for more details.
+
+ You should have received a copy of the GNU General Public License along 
+ with this program; if not, write to the Free Software Foundation, Inc., 
+ 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+
+CREATE DATABASE IF NOT EXISTS `genhotel` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE genhotel;
---
+++ 02_Tables.sql
@@ -0,0 +1,117 @@
+-- Copyright © 2014-2017, Michael Francisco / FCS. All rights reserved.
+/*
+ GenHotel database scripts for MySQL is licensed under the terms of the GPLv2
+ <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
+
+ This program is free software; you can redistribute it and/or modify 
+ it under the terms of the GNU General Public License as published 
+ by the Free Software Foundation; version 2 of the License.
+
+ This program is distributed in the hope that it will be useful, but 
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ or FITNESS FOR A PARTICULAR BUSINESS MODEL. See the GNU General Public License 
+ for more details.
+
+ You should have received a copy of the GNU General Public License along 
+ with this program; if not, write to the Free Software Foundation, Inc., 
+ 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+CREATE DATABASE IF NOT EXISTS `genhotel` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE genhotel;
+
+CREATE TABLE IF NOT EXISTS `booking` (
+  `booking_id` char(36) NOT NULL,
+  `guest_id` char(36) DEFAULT NULL,
+  `room_id` char(36) DEFAULT NULL,
+  `checkin` datetime DEFAULT NULL,
+  `checkout` datetime DEFAULT NULL,
+  `status` varchar(2) DEFAULT NULL,
+  PRIMARY KEY (`booking_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `guest` (
+  `guest_id` char(36) NOT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `nationality_id` int(2) DEFAULT NULL,
+  `passport_no` varchar(10) DEFAULT NULL,
+  `email` varchar(245) DEFAULT NULL,
+  `address1` varchar(50) DEFAULT NULL,
+  `address2` varchar(50) DEFAULT NULL,
+  `contact_no` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`guest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `mnt_department` (
+  `dept_id` char(36) NOT NULL,
+  `dept_description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`dept_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `mnt_room_type` (
+  `room_type_id` char(36) NOT NULL,
+  `room_type_description` varchar(45) DEFAULT NULL,
+  `default_fee` decimal(7,2) DEFAULT NULL,
+  PRIMARY KEY (`room_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `mnt_service_type` (
+  `service_type_id` char(36) NOT NULL,
+  `service_type_description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`service_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `mnt_serviceitem_type` (
+  `serviceitem_type_id` char(36) NOT NULL,
+  `service_type_id` char(36) DEFAULT NULL,
+  `serviceitem_description` varchar(50) DEFAULT NULL,
+  `serviceitem_defaultfee` decimal(7,2) DEFAULT NULL,
+  PRIMARY KEY (`serviceitem_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `mnt_stafftype` (
+  `stafftype_id` char(36) NOT NULL,
+  `dept_id` char(36) DEFAULT NULL,
+  `pay` decimal(7,2) DEFAULT NULL,
+  PRIMARY KEY (`stafftype_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `mtn_staff` (
+  `staff_id` char(36) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `address1` varchar(50) DEFAULT NULL,
+  `address2` varchar(50) DEFAULT NULL,
+  `contact_no` varchar(12) DEFAULT NULL,
+  `stafftype_id` char(36) DEFAULT NULL,
+  PRIMARY KEY (`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `reservation_id` char(36) NOT NULL,
+  `guest_id` char(36) DEFAULT NULL,
+  `booking_date` datetime DEFAULT NULL,
+  `room_type` varchar(2) DEFAULT NULL,
+  `confirmation_code` varchar(36) DEFAULT NULL,
+  PRIMARY KEY (`reservation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `room` (
+  `room_id` char(36) NOT NULL,
+  `room_type_id` char(36) DEFAULT NULL,
+  `fee` decimal(6,0) DEFAULT NULL,
+  `current_booking_id` char(36) DEFAULT NULL,
+  PRIMARY KEY (`room_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `service` (
+  `serviceitem_id` char(36) NOT NULL,
+  `booking_id` char(36) DEFAULT NULL,
+  `serviceitem_type_id` char(36) DEFAULT NULL,
+  `fee` decimal(7,2) DEFAULT NULL,
+  `discount` decimal(7,2) DEFAULT NULL,
+  `date_added` datetime DEFAULT NULL,
+  PRIMARY KEY (`serviceitem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
